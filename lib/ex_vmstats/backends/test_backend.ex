5daad325e8c9c2c69c60fe8d99d4b532d81d31ad
Ben Hallett <ben.hallett@jobseeker.com.au>
2017-01-17 16:57:40 +1100

    Cleaned up test backend

--- lib/ex_vmstats/backends/test_backend.ex
+++ lib/ex_vmstats/backends/test_backend.ex
@@ -1,31 +1,19 @@
 defmodule ExVmstats.TestBackend do
   require Logger
 
-  def counter(value, metric) do
-    Logger.info "counter #{inspect {value, metric}}"
-  end
-  def counter(value, metric, opts) do
+  def counter(value, metric, opts \\ []) do
     Logger.info "counter #{inspect {value, metric}} with opts #{inspect opts}"
   end
 
-  def gauge(value, metric) do
-    Logger.info "gauge #{inspect {value, metric}}"
-  end
-  def gauge(value, metric, opts) do
+  def gauge(value, metric, opts \\ []) do
     Logger.info "gauge #{inspect {value, metric}} with opts #{inspect opts}"
   end
 
-  def histogram(value, metric) do
-    Logger.info "histogram #{inspect {value, metric}}"
-  end
-  def histogram(value, metric, opts) do
+  def histogram(value, metric, opts \\ []) do
     Logger.info "histogram #{inspect {value, metric}} with opts #{inspect opts}"
   end
 
-  def timer(value, metric) do
-    Logger.info "timer #{inspect {value, metric}}"
-  end
-  def timer(value, metric, opts) do
+  def timer(value, metric, opts \\ []) do
     Logger.info "timer #{inspect {value, metric}} with opts #{inspect opts}"
   end
 end
