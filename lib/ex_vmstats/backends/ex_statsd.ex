2b9f0c8676b3c7e3ff5ecc618768a805768ff4a5
Ben Hallett <ben.hallett@jobseeker.com.au>
2017-01-17 16:33:24 +1100

    Backends to support options (for tags)

--- lib/ex_vmstats/backends/ex_statsd.ex
+++ lib/ex_vmstats/backends/ex_statsd.ex
@@ -1,17 +1,21 @@
 defmodule ExVmstats.Backends.ExStatsD do
-  def counter(value, metric) do
-    ExStatsD.counter(value, metric, sample_size: 1)
+  def counter(value, metric, opts \\ default_options()) do
+    ExStatsD.counter(value, metric, merge_opts(opts))
   end
 
-  def gauge(value, metric) do
-    ExStatsD.gauge(value, metric, sample_size: 1)
+  def gauge(value, metric, opts \\ default_options) do
+    ExStatsD.gauge(value, metric, merge_opts(opts))
   end
 
-  def histogram(value, metric) do
-    ExStatsD.histogram(value, metric, sample_size: 1)
+  def histogram(value, metric, opts \\ default_options) do
+    ExStatsD.histogram(value, metric, merge_opts(opts))
   end
 
-  def timer(value, metric) do
-    ExStatsD.timer(value, metric, sample_size: 1)
+  def timer(value, metric, opts \\ default_options) do
+    ExStatsD.timer(value, metric, merge_opts(opts))
   end
+
+  defp merge_opts(opts), do: Keyword.merge(opts, default_options, fn _k, v1, _v2 -> v1 end)
+
+  defp default_options, do: [sample_rate: 1, sample_size: 1, tags: []]
 end
